0411c29f7c648ddcd5b6a1ecf67f2e7609f24f17
neillsilva <blackvector@gmail.com>
2015-01-26 06:02:23 -0500

    Add streaming widget.

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -2,6 +2,10 @@ class SoundsController < ApplicationController
     def index
         @user = current_user
         client = Soundcloud.new(access_token: current_user.token)
+        playlist = client.get('/me/playlists/soundloved-tracks')
+        # Oembed seems cool, but this is printing literal HTML in the view.
+        #track_url = 'http://soundcloud.com/forss/flickermood'
+        #@embed_info = client.get('/oembed', :url => track_url)
 
         if current_user.playlist_created == false
             client.post('/playlists', playlist: {
